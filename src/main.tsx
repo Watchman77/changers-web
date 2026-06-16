36f77ebe056b6f169690ddb78507881e3a5791f0
BAMIDELE AKINWUMI <119769750+Watchman77@users.noreply.github.com>
2026-06-17 00:07:29 +0100

    Wire premium facelift into homepage

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { applyPremiumFacelift } from './premium-facelift'
+import './styles.css'
+import './premium-facelift.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+)
+
+applyPremiumFacelift()
