e450693522a051db7b7aa2f5e7a222009a0bc66c
BAMIDELE AKINWUMI <119769750+Watchman77@users.noreply.github.com>
2026-06-17 00:50:09 +0100

    Render premium homepage directly

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import { applyPremiumFacelift } from './premium-facelift'
-import './styles.css'
-import './premium-facelift.css'
+import PremiumChangersSite from './PremiumChangersSite'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PremiumChangersSite />
   </React.StrictMode>,
 )
-
-applyPremiumFacelift()
